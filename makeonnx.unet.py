bf17131ae79283d034046e04bbfdcab880d3d438
cangyeone <305601470@qq.com>
2025-10-27 09:36:44 +0800

    add new pickers

--- makeonnx.unet.py
+++ makeonnx.unet.py
@@ -1,36 +1,17 @@
-from models.UNet import UNet as Model  
+from models.UNet import PhaseNetLight 
 import torch 
 
-class Picker(Model):
+class Picker(PhaseNetLight):
     def __init__(self):
         super().__init__() 
-    def forward2(self, x):
-        #x = x.unsqueeze(3)
-        x = self.inputs(x)
-        x1 = self.layer0(x)
-        x2 = self.layer1(x1)
-        x3 = self.layer2(x2)
-        x4 = self.layer3(x3)
-        x5 = self.layer4(x4) 
-        x6 = self.layer5(x5)
-        x6 = torch.cat([x4, x6], dim=1) # 加入skip connection
-        x7 = self.layer6(x6)
-        x7 = torch.cat([x3, x7], dim=1) # 加入skip connection
-        x8 = self.layer7(x7)
-        x8 = torch.cat([x2, x8], dim=1) # 加入skip connection
-        x9 = self.layer8(x8)
-        x9 = torch.cat([x1, x9], dim=1) # 加入skip connection
-        x10 = self.layer9(x9)
-        x10 = x10.softmax(dim=1)
-        #x10 = x10.squeeze(dim=3)
-        return x10    
+   
     def forward(self, x):
         device = x.device
         with torch.no_grad():
             #print("数据维度", x.shape)
             T, C = x.shape 
-            seqlen = 6144 
-            batchstride = 6144 - 256
+            seqlen = 3072 
+            batchstride = seqlen - 3072 // 2
             batchlen = torch.ceil(torch.tensor(T / batchstride).to(device))
             idx = torch.arange(0, seqlen, 1, device=device).unsqueeze(0) + torch.arange(0, batchlen, 1, device=device).unsqueeze(1) * batchstride 
             idx = idx.clamp(min=0, max=T-1).long()
@@ -39,39 +20,38 @@ class Picker(Model):
             wave = wave.permute(0, 2, 1)
             wave -= torch.mean(wave, dim=2, keepdim=True)
             max, maxidx = torch.max(torch.abs(wave), dim=2, keepdim=True) 
+            max = torch.std(wave, dim=2, keepdim=True)
             wave /= (max + 1e-6)  
-            x = wave.unsqueeze(3)
-            x = self.inputs(x)
-            x1 = self.layer0(x)
-            x2 = self.layer1(x1)
-            x3 = self.layer2(x2)
-            x4 = self.layer3(x3)
-            x5 = self.layer4(x4) 
-            x6 = self.layer5(x5)
-            x6 = torch.cat([x4, x6], dim=1) # 加入skip connection
-            x7 = self.layer6(x6)
-            x7 = torch.cat([x3, x7], dim=1) # 加入skip connection
-            x8 = self.layer7(x7)
-            x8 = torch.cat([x2, x8], dim=1) # 加入skip connection
-            x9 = self.layer8(x8)
-            x9 = torch.cat([x1, x9], dim=1) # 加入skip connection
-            x10 = self.layer9(x9)
-            x10 = x10.softmax(dim=1)
-            oc = x10.squeeze(dim=3)
+            x_in = self.activation(self.in_bn(self.inc(wave)))
+            x1 = self.activation(self.bnd1(self.conv1(x_in)))
+            x2 = self.activation(self.bnd2(self.conv2(x1)))
+            x3 = self.activation(self.bnd3(self.conv3(x2)))
+            x4 = self.activation(self.bnd4(self.conv4(x3)))
+            
+            x = torch.cat([self.activation(self.bnu1(self.up1(x4))), x3], dim=1)
+            x = torch.cat([self.activation(self.bnu2(self.up2(x))), x2], dim=1)
+            x = torch.cat([self.activation(self.bnu3(self.up3(x))), x1], dim=1)
+            x = torch.cat([self.activation(self.bnu4(self.up4(x))), x_in], dim=1)
+            #print(x.shape)
+            x = self.out(x)
+            oc = self.softmax(x) 
             B, C, T = oc.shape 
             tgrid = torch.arange(0, T, 1, device=device).unsqueeze(0) * 1 + torch.arange(0, batchlen, 1, device=device).unsqueeze(1) * batchstride
             oc = oc.permute(0, 2, 1).reshape(-1, C) 
+            oc = oc[:, [2, 0, 1]]
             ot = tgrid.squeeze()
             ot = ot.reshape(-1) 
         return oc, ot   
 model = Picker() 
 model.eval()
-model.load_state_dict(torch.load("ckpt/china.unet.pt", map_location="cpu"))
+ckpt = torch.load("model_list/9_sc.pt", weights_only=False, map_location="cpu")
+state = ckpt.state_dict()
+model.load_state_dict(state)
 input_names = ["wave"]
 output_names = ["prob", "time"]
 #x = torch.randn([10, 3, 6144, 1])
 x = torch.randn([500000, 3])
 torch.onnx.export(model, x, 
-"pickers/unet.onnx", verbose=True, 
+"pickers/9_sc.onnx", verbose=True, 
 dynamic_axes={"wave":{0:"batch"}, "prob":{0:"batch"}, "time":{0:"batch"}}, 
 input_names=input_names, output_names=output_names, opset_version=11)
