89c4696226a6fd82291ecbac622d231751b13935
cangyeone <305601470@qq.com>
2026-02-04 15:28:39 +0800

    Update Picker initialization and JIT save filename

--- makejit.pnsn.py
+++ makejit.pnsn.py
@@ -5,11 +5,11 @@ from jit_picker_base import SlidingWindowPicker
 
 class Picker(SlidingWindowPicker):
     def __init__(self):
-        super().__init__(BRNN, ckpt_path="ckpt/china.rnn.pnsn.pt")
+        super().__init__(BRNN, ckpt_path="ckpt/china.rnn.pnsn.pt", norm="std", diff=False, seqlen=10240, overlap=1024, threshold=0.1, min_gap=300)
 
 
 model = Picker()
 model.eval()
-torch.jit.save(torch.jit.script(model), "pickers/pnsn.jit")
+torch.jit.save(torch.jit.script(model), "pickers/pnsn.v1.jit")
 x = torch.randn([300000, 3])
 y = model(x)
