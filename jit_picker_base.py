91991242f0e46c93c265fbaf73d01ace4f320228
cangyeone <305601470@qq.com>
2026-02-04 15:28:14 +0800

    Update jit_picker_base.py

--- jit_picker_base.py
+++ jit_picker_base.py
@@ -11,8 +11,8 @@ class SlidingWindowPicker(nn.Module):
     the prefix are supported by automatically adding it during load.
     """
 
-    def __init__(self, model_ctor, ckpt_path=None, *, state_dict=None,
-                 seqlen=6144, overlap=256, threshold=0.3, min_gap=1000):
+    def __init__(self, model_ctor, ckpt_path=None, *, norm="std", diff=False, state_dict=None,
+                 seqlen=6144, overlap=256, threshold=0.1, min_gap=1000):
         super().__init__()
         self.model = model_ctor()
         self.n_stride = 1
@@ -20,7 +20,8 @@ class SlidingWindowPicker(nn.Module):
         self.batchstride = seqlen - overlap
         self.threshold = threshold
         self.min_gap = min_gap
-
+        self.diff = diff 
+        self.norm = norm # "std" or "max"
         if state_dict is None:
             if ckpt_path is None:
                 raise ValueError("Either ckpt_path or state_dict must be provided")
@@ -39,18 +40,23 @@ class SlidingWindowPicker(nn.Module):
                 + torch.arange(0, batchlen, 1, device=device).unsqueeze(1)
                 * self.batchstride
             )
-            idx = idx.clamp(min=0, max=T - 1).long()
+            idx = idx.clamp(min=0, max=T - 2).long()
+
+            
             wave = x.to(device)[idx, :]
             wave = wave.permute(0, 2, 1)
             wave -= torch.mean(wave, dim=2, keepdim=True)
-            maxv, _ = torch.max(torch.abs(wave), dim=2, keepdim=True)
+            if self.norm=="std":
+                maxv = torch.std(wave, dim=2, keepdim=True)
+            else:
+                maxv, _ = torch.max(torch.abs(wave), dim=2, keepdim=True)
             wave /= (maxv + 1e-6)
 
             logits = self.model(wave)
             if logits.dim() == 4:
                 logits = logits.squeeze(dim=3)
-            if logits.shape[1] > 1:
-                logits = logits.softmax(dim=1)
+            #if logits.shape[1] > 1:
+            #    logits = logits.softmax(dim=1)
 
             B, C, T = logits.shape
             tgrid = (
@@ -60,8 +66,25 @@ class SlidingWindowPicker(nn.Module):
             oc = logits.permute(0, 2, 1).reshape(-1, C)
             ot = tgrid.squeeze().reshape(-1)
 
+            if self.diff:
+                # 2nd pass for diff
+                wave = (x[1:]-x[:-1])[idx, :]
+                wave = wave.permute(0, 2, 1)
+                wave -= torch.mean(wave, dim=2, keepdim=True)
+                if self.norm=="std":
+                    maxv = torch.std(wave, dim=2, keepdim=True)
+                else:
+                    maxv, _ = torch.max(torch.abs(wave), dim=2, keepdim=True)
+                #max, maxidx = torch.max(torch.abs(wave), dim=2, keepdim=True) 
+                wave /= (maxv + 1e-6)  
+                logits = self.model(wave)
+                #oc2 = y.softmax(dim=1)
+                oc2 = logits.permute(0, 2, 1).reshape(-1, C)
+                oc = torch.cat([oc, oc2], dim=0)
+                ot = torch.cat([ot, ot], dim=0) 
+            #print(oc.shape, ot.shape)
             output = []
-            for itr in range(2):
+            for itr in range(C-1):
                 pc = oc[:, itr + 1]
                 time_sel = torch.masked_select(ot, pc > self.threshold)
                 score = torch.masked_select(pc, pc > self.threshold)
